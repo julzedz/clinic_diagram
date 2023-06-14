85863602f69653e30e45e03f0258e667b14c7be2
user <user@users-MacBook-Air.local>
2023-06-14 11:40:14 +0100

    Effect corrections

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,17 +1,17 @@
 CREATE TABLE patients(
-    id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
     date_of_birth DATE,
 );
 
 CREATE TABLE treatments(
-    id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     type VARCHAR(255),
     name VARCHAR(255),
 );
 
 CREATE TABLE medical_histories(
-    id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     admitted_at TIMESTAMP,
     patient_id INTEGER,
     status VARCHAR(255),
@@ -21,10 +21,11 @@ CREATE TABLE medical_histories(
 CREATE TABLE medical_histories_treatments(
     CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
     CONSTRAINT fk_treatment FOREIGN KEY(treatments_id) REFERENCES treatments(id),
+    PRIMARY KEY (medical histories_id, treatments_id)
 );
 
 CREATE TABLE invoices(
-    id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     total_amount DECIMAL(8, 2),
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
@@ -33,7 +34,7 @@ CREATE TABLE invoices(
 );
 
 CREATE TABLE invoice_items(
-    id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     unit_price DECIMAL(8, 2),
     quantity INTEGER,
     total_price DECIMAL(8, 2),
